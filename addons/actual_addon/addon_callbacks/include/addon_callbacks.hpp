1a40b0b9a1d286bbf9f6c2b0c191a6212b39b262
lurkydismal <lurkydismal@duck.com>
2024-04-24 10:33:41 -0700

    123

--- addons/actual_addon/addon_callbacks/include/addon_callbacks.hpp
+++ addons/actual_addon/addon_callbacks/include/addon_callbacks.hpp
@@ -3,12 +3,4 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-#include <cstdint>
-
 extern HWND g_hFocusWindow;
-
-extern "C" {
-
-uint16_t __declspec( dllexport )
-    IDirect3D9Ex$CreateDevice( void** _callbackArguments );
-}
