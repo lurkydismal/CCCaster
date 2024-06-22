2db00e93e47a6579df60806e98a483159b3cbe97
LurkyDismal <lurkydismal@duck.com>
2024-06-22 11:43:18 +0000

    123

--- addons/keyboard_addon/src/main.cpp
+++ addons/keyboard_addon/src/main.cpp
@@ -36,89 +36,95 @@ BOOL WINAPI DllMain( HMODULE _moduleHandle, DWORD _callReason, LPVOID _ ) {
                           std::string( CONTROLS_PREFERENCES_FILE_EXTENSION ) );
 
                     // Parse controls preferences file
-                    std::ifstream l_jsonFileIn( l_controlsConfigFileName );
-
-                    if ( l_jsonFileIn.is_open() ) {
+                    try {
+                        // parsing input with a syntax error
                         MessageBoxA( 0, "1", "test", 0 );
 
-                        try {
-                            // parsing input with a syntax error
-                            MessageBoxA( 0, "1", "test", 0 );
+                        char* buffer = 0;
+                        long length;
+                        FILE* f =
+                            fopen( l_controlsConfigFileName.c_str(), "rb" );
 
-                            g_jsonControlsKeyboard =
-                                json::parse( l_jsonFileIn );
-
-                        } catch ( const json::parse_error& e ) {
-                            // output exception information
-                            MessageBoxA( 0, std::string( e.what() ).c_str(),
-                                         "test", 0 );
+                        if ( f ) {
+                            fseek( f, 0, SEEK_END );
+                            length = ftell( f );
+                            fseek( f, 0, SEEK_SET );
+                            buffer = ( char* )malloc( length );
+                            if ( buffer ) {
+                                fread( buffer, 1, length, f );
+                            }
+                            fclose( f );
                         }
 
-                    } else {
-                        MessageBoxA( 0, "2", "test", 0 );
+                        std::string t;
+                        if ( buffer ) {
+                            t = std::string( buffer );
+                            free( buffer );
 
-                        std::ifstream l_jsonBackupFileIn(
-                            l_controlsConfigBackupFileName );
-
-                        std::ostringstream sstr;
-                        sstr << l_jsonBackupFileIn.rdbuf();
-
-                        MessageBoxA( 0, sstr.str().c_str(), "test", 0 );
-
-                        if ( l_jsonBackupFileIn.is_open() ) {
-                            MessageBoxA( 0, "3", "test", 0 );
-
-                            try {
-                                // parsing input with a syntax error
-                                MessageBoxA( 0, "7", "test", 0 );
-
-                                g_jsonControlsKeyboard =
-                                    json::parse( l_jsonBackupFileIn );
-
-                            } catch ( const json::parse_error& e ) {
-                                // output exception information
-                                MessageBoxA( 0, std::string( e.what() ).c_str(),
-                                             "test", 0 );
-                            }
+                            MessageBoxA( 0, "2", "test", 0 );
+                            MessageBoxA( 0, t.c_str(), "test", 0 );
 
                         } else {
-                            MessageBoxA( 0, "4", "test", 0 );
-                            g_jsonControlsKeyboard = {
-                                { "38", "8" },
-                                { "39", "6" },
-                                { "40", "2" },
-                                { "37", "4" },
-                                { "90", "A" },
-                                { "88", "B" },
-                                { "67", "C" },
-                                { "86", "D" },
-                                { "68", "E" },
-                                { "83", "AB" },
-                                { "221", "FN1" },
-                                { "82", "FN2" },
-                                { "84", "START" },
-                                { "115", "ToggleOverlay_KeyConfig_Native" },
-                            };
+                            buffer = 0;
+                            f = fopen( l_controlsConfigBackupFileName.c_str(),
+                                       "rb" );
+
+                            if ( f ) {
+                                fseek( f, 0, SEEK_END );
+                                length = ftell( f );
+                                fseek( f, 0, SEEK_SET );
+                                buffer = ( char* )malloc( length );
+                                if ( buffer ) {
+                                    fread( buffer, 1, length, f );
+                                }
+                                fclose( f );
+                            }
+
+                            if ( buffer ) {
+                                t = std::string( buffer );
+                                free( buffer );
+
+                                MessageBoxA( 0, "3", "test", 0 );
+                                MessageBoxA( 0, t.c_str(), "test", 0 );
+                            }
                         }
 
-                        std::ofstream l_jsonFileOut( l_controlsConfigFileName );
+                        MessageBoxA( 0, "6", "test", 0 );
+                        MessageBoxA( 0, t.c_str(), "test", 0 );
+                        g_jsonControlsKeyboard = json::parse( t );
 
-                        if ( l_jsonFileOut.is_open() ) {
-                            MessageBoxA( 0, "6", "test", 0 );
+                    } catch ( const json::parse_error& e ) {
+                        // output exception information
+                        MessageBoxA( 0, std::string( e.what() ).c_str(), "test",
+                                     0 );
 
-                            try {
-                                // parsing input with a syntax error
-                                MessageBoxA( 0, "8", "test", 0 );
+                        MessageBoxA( 0, "4", "test", 0 );
+                        g_jsonControlsKeyboard = {
+                            { "38", "8" },
+                            { "39", "6" },
+                            { "40", "2" },
+                            { "37", "4" },
+                            { "90", "A" },
+                            { "88", "B" },
+                            { "67", "C" },
+                            { "86", "D" },
+                            { "68", "E" },
+                            { "83", "AB" },
+                            { "221", "FN1" },
+                            { "82", "FN2" },
+                            { "84", "START" },
+                            { "115", "ToggleOverlay_KeyConfig_Native" },
+                        };
 
-                                l_jsonFileOut
-                                    << g_jsonControlsKeyboard.dump( 4 )
-                                    << std::endl;
+                        std::string t = g_jsonControlsKeyboard.dump( 4 ) +
+                                        std::string( "\n" );
 
-                            } catch ( const json::parse_error& e ) {
-                                // output exception information
-                                MessageBoxA( 0, std::string( e.what() ).c_str(),
-                                             "test", 0 );
-                            }
+                        const char* buffer = t.c_str();
+                        FILE* f =
+                            fopen( l_controlsConfigFileName.c_str(), "wb" );
+
+                        if ( f ) {
+                            fprintf( f, "%s\n", buffer );
                         }
 
                         MessageBoxA( 0, "5", "test", 0 );
