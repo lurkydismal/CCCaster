e6bf72735ad40e50dea6cb982bd5266d92c85646
lurkydismal <lurkydismal@duck.com>
2024-07-14 10:45:48 +0000

    123

--- addons/example/example.c
+++ addons/example/example.c
@@ -12,30 +12,36 @@
 
 extern "C" int16_t __declspec( dllexport )
     onGameStarted( void** _callbackArguments ) {
-    MessageBoxA( NULL,
+    int16_t l_returnValue = 0;
+
+    l_returnValue = !( MessageBoxA( NULL,
                  "GameStarted", // box text
                  "on",          // box name
-                 0 );
+                 0 ) );
 
-    return ( 0 );
+    return ( l_returnValue );
 }
 
 extern "C" int16_t __declspec( dllexport )
     onHosted( void** _callbackArguments ) {
-    MessageBoxA( NULL,
+    int16_t l_returnValue = 0;
+
+    l_returnValue = !( MessageBoxA( NULL,
                  "Hosted", // box text
                  "on",     // box name
-                 0 );
+                 0 ) );
 
-    return ( 0 );
+    return ( l_returnValue );
 }
 
 extern "C" int16_t __declspec( dllexport )
     onConnection( void** _callbackArguments ) {
-    MessageBoxA( NULL,
+    int16_t l_returnValue = 0;
+
+    l_returnValue = !( MessageBoxA( NULL,
                  "Connection", // box text
                  "on",         // box name
-                 0 );
+                 0 ) );
 
-    return ( 0 );
+    return ( l_returnValue );
 }
