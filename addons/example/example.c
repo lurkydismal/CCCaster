a729ae4cdf7a4ce76fd79448c0162720be5f8617
lurkydismal <lurkydismal@duck.com>
2024-07-14 10:47:18 +0000

    123

--- addons/example/example.c
+++ addons/example/example.c
@@ -10,38 +10,38 @@
 
 #include <cstdint>
 
-extern "C" int16_t __declspec( dllexport )
-    onGameStarted( void** _callbackArguments ) {
+extern "C" int16_t __declspec( dllexport ) onGameStarted(
+    void** _callbackArguments ) {
     int16_t l_returnValue = 0;
 
     l_returnValue = !( MessageBoxA( NULL,
-                 "GameStarted", // box text
-                 "on",          // box name
-                 0 ) );
+                                    "GameStarted", // box text
+                                    "on",          // box name
+                                    0 ) );
 
     return ( l_returnValue );
 }
 
-extern "C" int16_t __declspec( dllexport )
-    onHosted( void** _callbackArguments ) {
+extern "C" int16_t __declspec( dllexport ) onHosted(
+    void** _callbackArguments ) {
     int16_t l_returnValue = 0;
 
     l_returnValue = !( MessageBoxA( NULL,
-                 "Hosted", // box text
-                 "on",     // box name
-                 0 ) );
+                                    "Hosted", // box text
+                                    "on",     // box name
+                                    0 ) );
 
     return ( l_returnValue );
 }
 
-extern "C" int16_t __declspec( dllexport )
-    onConnection( void** _callbackArguments ) {
+extern "C" int16_t __declspec( dllexport ) onConnection(
+    void** _callbackArguments ) {
     int16_t l_returnValue = 0;
 
     l_returnValue = !( MessageBoxA( NULL,
-                 "Connection", // box text
-                 "on",         // box name
-                 0 ) );
+                                    "Connection", // box text
+                                    "on",         // box name
+                                    0 ) );
 
     return ( l_returnValue );
 }
