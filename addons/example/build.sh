38c75954d3625eb996a690e8cf670b7ce8cffcf8
lurkydismal <lurkydismal@duck.com>
2024-07-14 10:44:11 +0000

    123

--- addons/example/build.sh
+++ addons/example/build.sh
@@ -4,11 +4,11 @@ source ../../config.sh && \
 clear && \
 mkdir -p "${ADDONS_DIR}/example" && \
     make clean && \
-    make -j $( ( `nproc` - 1 )) && \
+    make -j $(( `nproc` - 1 )) && \
     mv -f example.dll.so \
     "${ADDONS_DIR}/example/example.dll" && \
     cp -f info.hjson \
     "${ADDONS_DIR}/example/." && \
     clang-format --style=file \
     -i \
-    example.cpp
+    example.c
