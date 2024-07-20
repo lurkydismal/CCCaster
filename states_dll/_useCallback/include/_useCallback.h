1bd81fb832920b07d3d7f235a2c7bf64c2eb0bd5
lurkydismal <lurkydismal@duck.com>
2024-07-20 12:40:12 +0000

    123

--- states_dll/_useCallback/include/_useCallback.h
+++ states_dll/_useCallback/include/_useCallback.h
@@ -2,6 +2,13 @@
 
 #include <stdint.h>
 
+#define ARGUMENTS_COUNT( ... ) \
+    ( ( sizeof( ( const void*[] ){ NULL, ##__VA_ARGS__ } ) / sizeof( const void* ) ) - 1 )
+#define _useCallback( _callbackName, ... )                         \
+    __useCallback( _callbackName,                                  \
+                   ( const size_t )ARGUMENTS_COUNT( __VA_ARGS__ ), \
+                   ##__VA_ARGS__ )
+
 #if defined( __cplusplus )
 
 extern "C" {
@@ -12,9 +19,9 @@ typedef uint16_t( __cdecl* useCallbackFunction_t )( const char* _callbackName,
                                                     void** _callbackArguments );
 extern useCallbackFunction_t g_useCallback;
 
-uint16_t _useCallback( const char* _callbackName,
-                       const size_t _callbackArgumentsCount,
-                       ... );
+uint16_t __useCallback( const char* _callbackName,
+                        const size_t _callbackArgumentsCount,
+                        ... );
 
 #if defined( __cplusplus )
 }
