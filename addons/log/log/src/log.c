a0900baf59048458b589d7ab5056bb26d2b61dff
LurkyDismal <lurkydismal@duck.com>
2024-07-27 14:42:54 +0000

    123

--- addons/log/log/src/log.c
+++ addons/log/log/src/log.c
@@ -1,7 +1,7 @@
 #include "log.h"
 
-#include <string.h>
 #include <stdarg.h>
+#include <string.h>
 
 static char g_transactionString[ 1024 * 2 ];
 static size_t g_transactionStringLength = 0;
@@ -9,7 +9,7 @@ static size_t g_transactionStringLength = 0;
 uint16_t log_query( const char* _string, const size_t _stringLength ) {
     uint16_t l_returnValue = 0;
 
-    memcpy( g_transactionString + g_transactionStringLength - 1, _string,
+    memcpy( g_transactionString + g_transactionStringLength, _string,
             _stringLength );
     g_transactionStringLength += _stringLength;
 
@@ -17,24 +17,19 @@ uint16_t log_query( const char* _string, const size_t _stringLength ) {
 }
 
 uint16_t log_commit( void ) {
-    uint16_t l_returnValue = 0;
+    uint16_t l_returnValue = 1;
 
     if ( g_transactionStringLength ) {
         size_t l_writtenCount =
             fwrite( g_transactionString, sizeof( g_transactionString[ 0 ] ),
                     g_transactionStringLength, g_fileDescriptor );
 
-        if ( l_writtenCount < g_transactionStringLength ) {
-            l_returnValue = 1;
-
-            goto EXIT;
-        }
+        l_returnValue = !( l_writtenCount == g_transactionStringLength );
 
         fflush( g_fileDescriptor );
 
         g_transactionStringLength = 0;
     }
 
-EXIT:
     return ( l_returnValue );
 }
--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-MBAA_DIR="/home/makar/Downloads/MBAACC"
+MBAA_DIR="/mnt/MBAACC - Community Edition/mbaacc2/game_info/data/MBAACC"
 ADDONS_DIR="${MBAA_DIR}/addons"
