79f7ef724b5cd80ecb03702f51aaa069ef1478a9
LurkyDismal <lurkydismal@duck.com>
2024-08-11 16:30:23 +0000

    123

--- addons/keyboard_addon/build.sh
+++ addons/keyboard_addon/build.sh
@@ -9,8 +9,6 @@ mkdir -p "${ADDONS_DIR}/keyboard" && \
     "${ADDONS_DIR}/keyboard/keyboard.dll" && \
     cp -f info.hjson \
     "${ADDONS_DIR}/keyboard/." && \
-    cp -f controls_backup.json \
-    "${MBAA_DIR}/." && \
     clang-format --style=file \
     -i \
     addon_callbacks/src/*.c addon_callbacks/include/*.h \
