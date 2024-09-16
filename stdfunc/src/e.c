d9b1ba6639b8e91f2d9bba18845c8ce05713f245
lurkydismal <lurkydismal@duck.com>
2024-09-16 11:43:49 +0500

    321

--- stdfunc/src/e.c
+++ stdfunc/src/e.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
