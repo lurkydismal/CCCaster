922b17bc1fbf99aac933c62da414e9713591d9e7
lurkydismal <lurkydismal@duck.com>
2024-11-18 14:22:20 +0500

    321

--- addons/overlay_addon/overlay/src/interact_element.c
+++ addons/overlay_addon/overlay/src/interact_element.c
@@ -10,12 +10,15 @@
 
 #include "stdfunc.h"
 
-uint16_t interactElement( const element_t* _element, char*** _activeMappedKeys, char*** _activeKeys );
+uint16_t interactElement( const element_t* _element,
+                          char*** _activeMappedKeys,
+                          char*** _activeKeys ) {
     uint16_t l_returnValue = 0;
 
     switch ( _element->type ) {
         default: {
-            l_returnValue = _useCallback( "overlay$interact$unknown", _element, _activeMappedKeys, _activeKeys );
+            l_returnValue = _useCallback( "overlay$interact$unknown", _element,
+                                          _activeMappedKeys, _activeKeys );
         }
     }
 
