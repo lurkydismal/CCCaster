4ad5e3a62409b042ed49d2939153a7b97961051d
LurkyDismal <lurkydismal@duck.com>
2026-03-25 12:50:15 +0000

    Launcher main no longer reuiqres .env

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,7 +1,6 @@
 use launcher::launcher_error;
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     match dotenvy::dotenv() {
         Ok(l_path) => {
             eprintln!(
@@ -13,9 +12,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             let l_cwd = std::env::current_dir()
                 .map(|l_path| l_path.display().to_string())
                 .unwrap_or_else(|l_cwd_err| format!("<unavailable: {l_cwd_err}>"));
-            eprintln!("[LAUNCHER] [ERROR] Failed to load .env (cwd: {l_cwd})");
+            eprintln!("[LAUNCHER] [WARN] Failed to load .env (cwd: {l_cwd})");
             eprintln!("[LAUNCHER] [ERROR] dotenv error kind: {:?}", l_err);
-            return Err(l_err.into());
         }
     }
 
@@ -23,6 +21,4 @@ fn main() -> Result<(), Box<dyn Error>> {
         launcher_error!("{l_err}");
         std::process::exit(1);
     }
-
-    Ok(())
 }
