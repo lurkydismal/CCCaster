d9613926240a08fcc9c5902279f28637eb40261b
LurkyDismal <lurkydismal@duck.com>
2026-03-26 13:59:29 +0000

    Revert "Do not extract verbose level and trace flag from json"
    This reverts commit 8fed165acd585aeb4fa83849a9e0a7bd7be54100.

--- wrapper/src/wrapper.cpp
+++ wrapper/src/wrapper.cpp
@@ -5,7 +5,9 @@
 
 #include <bit>
 #include <cctype>
+#include <cstdint>
 #include <cstdlib>
+#include <limits>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -45,7 +47,10 @@ struct std::formatter< data_t > {
 namespace {
 
 using wrapperData_t = struct wrapperData {
+    uint8_t verbose{};
+    bool trace{};
     bool timings{};
+
     bool no_patches{};
 };
 
@@ -228,13 +233,67 @@ auto parseBool( std::string_view _s, size_t& _i, bool& _out ) -> bool {
     return ( false );
 }
 
+auto parseUint8( std::string_view _s, size_t& _i, uint8_t& _out ) -> bool {
+    skipWs( _s, _i );
+
+    if ( _i >= _s.size() ) {
+        logg::trace( "parseUint8: input ended at {}", _i );
+        return ( false );
+    }
+
+    uint32_t l_value = 0;
+    size_t l_start = _i;
+    bool l_hasDigit = false;
+
+    while ( _i < _s.size() ) {
+        auto const l_c = static_cast< unsigned char >( _s[ _i ] );
+
+        if ( ( l_c < '0' ) || ( l_c > '9' ) ) {
+            break;
+        }
+
+        l_hasDigit = true;
+        l_value = ( l_value * 10u ) + static_cast< uint32_t >( l_c - '0' );
+
+        if ( l_value > static_cast< uint32_t >(
+                           std::numeric_limits< uint8_t >::max() ) ) {
+            logg::trace( "parseUint8: value overflow at {}", _i );
+            return ( false );
+        }
+
+        ++_i;
+    }
+
+    if ( !l_hasDigit ) {
+        logg::trace( "parseUint8: no digits at {}", l_start );
+        return ( false );
+    }
+
+    _out = static_cast< uint8_t >( l_value );
+
+    logg::trace( "parseUint8: parsed {} from [{}..{})",
+                 static_cast< unsigned >( _out ), l_start, _i );
+
+    return ( _i > l_start );
+}
+
 auto parseValueForKey( std::string_view _key,
                        std::string_view _s,
                        size_t& _i,
                        wrapperData_t& _out ) -> bool {
     logg::trace( "parseValueForKey: key='{}' at position {}", _key, _i );
 
-    if ( _key == "timings" ) {
+    if ( _key == "verbose" ) {
+        const bool l_result = parseUint8( _s, _i, _out.verbose );
+        logg::trace( "parseValueForKey: verbose -> {}", l_result );
+        return ( l_result );
+
+    } else if ( _key == "trace" ) {
+        const bool l_result = parseBool( _s, _i, _out.trace );
+        logg::trace( "parseValueForKey: trace -> {}", l_result );
+        return ( l_result );
+
+    } else if ( _key == "timings" ) {
         const bool l_result = parseBool( _s, _i, _out.timings );
         logg::trace( "parseValueForKey: timings -> {}", l_result );
         return ( l_result );
@@ -316,9 +375,10 @@ auto parseWrapperData( std::string_view _json )
     }
 
     logg::debug(
-        "parseWrapperData: success timings={}, "
+        "parseWrapperData: success verbose={}, trace={}, timings={}, "
         "no_patches={}",
-        l_out.timings, l_out.no_patches );
+        static_cast< unsigned >( l_out.verbose ), l_out.trace, l_out.timings,
+        l_out.no_patches );
 
     return ( l_out );
 }
@@ -446,18 +506,18 @@ auto attach() -> bool {
     l_attachTimer.setEnabled( g_timingsEnabled );
 
     logg::debug( "attach: timings enabled={}", g_timingsEnabled );
-    logg::debug( "attach: config timings={}, no_patches={}",
-                 l_wrapperData->timings, l_wrapperData->no_patches );
+    logg::debug(
+        "attach: config verbose={}, trace={}, timings={}, no_patches={}",
+        static_cast< unsigned >( l_wrapperData->verbose ), l_wrapperData->trace,
+        l_wrapperData->timings, l_wrapperData->no_patches );
 
     logg::debug( "SIZE: '{}', VALUE: '{}'", l_data->size,
                  std::string_view( l_data->value, l_data->size ) );
 
-    logg::trace( "CALLING INIT()" );
+    logg::info( "CALLING INIT()" );
 
     const bool l_result = l_initFunction(
-        ( l_wrapperData->no_patches ? nullptr : wrapper::makePatch ),
-        ( l_wrapperData->no_patches ? nullptr : wrapper::removePatch ),
-        l_data->value, l_data->size );
+        wrapper::makePatch, wrapper::removePatch, l_data->value, l_data->size );
 
     if ( l_result ) {
         logg::info( "CCCASTER LOADED" );
