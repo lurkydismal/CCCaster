c0246cec2c8219d002b7a8f4191e0d6e750c0e52
LurkyDismal <lurkydismal@duck.com>
2026-03-26 13:49:56 +0000

    Do not log anything in launcher about .env

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -2,19 +2,8 @@ use launcher::launcher_error;
 
 fn main() {
     match dotenvy::dotenv() {
-        Ok(l_path) => {
-            eprintln!(
-                "[LAUNCHER] [DEBUG] Loaded environment file: {}",
-                l_path.display()
-            );
-        }
-        Err(l_err) => {
-            let l_cwd = std::env::current_dir()
-                .map(|l_path| l_path.display().to_string())
-                .unwrap_or_else(|l_cwd_err| format!("<unavailable: {l_cwd_err}>"));
-            eprintln!("[LAUNCHER] [WARN] Failed to load .env (cwd: {l_cwd})");
-            eprintln!("[LAUNCHER] [ERROR] dotenv error kind: {:?}", l_err);
-        }
+        Ok(_path) => {}
+        Err(_e) => {}
     }
 
     if let Err(l_err) = launcher::run_cli() {
