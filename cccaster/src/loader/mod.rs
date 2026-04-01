0897b993695f01bb27899050aee6e8f266ad6766
LurkyDismal <lurkydismal@duck.com>
2026-04-01 18:05:22 +0000

    Remove unshare call from overlay vfs

--- cccaster/src/loader/mod.rs
+++ cccaster/src/loader/mod.rs
@@ -5,7 +5,4 @@ mod engine_require_dispatch;
 mod overlay_vfs;
 mod path_utils;
 
-pub use core::{
-    init_process_local_overlay, load_mods_from_addons, register_engine_variable,
-    shutdown_before_unload,
-};
+pub use core::{load_mods_from_addons, register_engine_variable, shutdown_before_unload};
